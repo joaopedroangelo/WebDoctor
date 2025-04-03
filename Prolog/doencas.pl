a6df4a01e601255923475b1194980a46f9f109a6
joao-pedro-angelo <carneiroangelojoaopedro@gmail.com>
2025-04-03 18:17:07 -0300

    cria as doenças

---
+++ Prolog/doencas.pl
@@ -0,0 +1,78 @@
+
+%colera
+sintoma(colera, diarreia).
+sintoma(colera, vomito).
+sintoma(colera, nausea).
+sintoma(colera, boca_seca).
+sintoma(colera, sede).
+
+%chicungunya
+sintoma(chicungunya, febre).
+sintoma(chicungunya, dor_de_cabeca).
+sintoma(chicungunya, dor_nas_articulacoes).
+sintoma(chicungunya, dor_muscular).
+sintoma(chicungunya, mialgia).
+
+%gripe
+sintoma(gripe, febre).
+sintoma(gripe, tosse).
+sintoma(gripe, dor_de_garganta).
+sintoma(gripe, fraqueza).
+sintoma(gripe, coriza).
+
+%pneumonia
+sintoma(pneumonia, febre).
+sintoma(pneumonia, tosse).
+sintoma(pneumonia, falta_de_ar).
+sintoma(pneumonia, fraqueza).
+sintoma(pneumonia, dor_no_peito).
+
+%raiva
+sintoma(raiva, febre).
+sintoma(raiva, convulsao).
+sintoma(raiva, dor_de_cabeca).
+sintoma(raiva, ansiedade).
+sintoma(raiva, salivacao).
+
+%hanseniase
+sintoma(hanseniase, manchas_na_pele).
+sintoma(hanseniase, perda_de_sensibilidade).
+sintoma(hanseniase, formigamento).
+sintoma(hanseniase, paralisia).
+sintoma(hanseniase, edema).
+
+%hepatite
+sintoma(hepatite, febre).
+sintoma(hepatite, falta_de_apetite).
+sintoma(hepatite, olhos_amarelados).
+sintoma(hepatite, urina_escura).
+sintoma(hepatite, mal_estar).
+
+%candidiase
+sintoma(candidiase, coceira).
+sintoma(candidiase, manchas_na_pele).
+sintoma(candidiase, dor_ao_urinar).
+sintoma(candidiase, corrimento).
+sintoma(candidiase, feridas).
+
+%parkinson
+sintoma(parkinson, tremor).
+sintoma(parkinson, rigidez).
+sintoma(parkinson, lentidao).
+sintoma(parkinson, alteracao_da_escrita).
+sintoma(parkinson, dificuldade_de_andar).
+
+%alzheimer
+sintoma(alzheimer, perda_de_memoria).
+sintoma(alzheimer, confusao).
+sintoma(alzheimer, irritabilidade).
+sintoma(alzheimer, apatia).
+sintoma(alzheimer, dificuldade_intelectual).
+
+%aids
+sintoma(aids, febre).
+sintoma(aids, fraqueza).
+sintoma(aids, perda_de_peso).
+sintoma(aids, suor_noturno).
+sintoma(aids, diarreia).
+
